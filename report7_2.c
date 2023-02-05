1ac6e25f0007bf0ebccd6bf4059449d8bf3d62cf
Yoshiharu Habu <e205731@ie.u-ryukyu.ac.jp>
2023-02-05 15:24:14 +0900

    bubble sort

---
+++ report7_2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+ 
+int main(void){
+    int i, j, num;
+    int scores[6] = {100, 60, 70, 100, 90, 80};
+    printf("scores=");
+    for (i=0; i<6; ++i){
+        printf("%d,", scores[i]);
+    }
+    printf("\n");
+    //数値を降べきの順にソートするためのfor文を作る
+    //入力された数字を全て比較する
+    for (i=0; i<6; ++i) {
+        for (j=0; j<6; ++j) {
+            //数字を比較し、数字を降べきの順に並び直す
+            //このif文によって適当に入力された数値を降べきの順に整理されたものをnumberという配列に保管し直す
+            if (scores[j+1] < scores[j]) {
+                num =  scores[j];
+                scores[j] = scores[j+1];
+                scores[j+1] = num;
+                }
+            }
+        }
+
+    //降べきの順にソートした数値を出力する
+    printf("results=");
+    for (i=0; i<6; ++i){
+        printf("%d,", scores[i]);
+    }
+    printf("\n");
+}
