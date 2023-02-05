e5dfe33efb051e4cac908e71fcc94439c41901e6
Yoshiharu Habu <e205731@ie.u-ryukyu.ac.jp>
2023-02-05 15:26:41 +0900

    Bubble Sort

--- report7.c
+++ report7.c
@@ -23,7 +23,7 @@ int main(void){
     for (i=0; i<total-1; ++i) {
         for (j=0; j<total-1; ++j) {
             //数字を比較し、数字を降べきの順に並び直す
-            //このif文によって適当に入力された数値を降べきの順に整理されたものをnumberという配列に保管し直す
+            //このif文によって適当に入力された数値を降べきの順に整理されたものをnumという配列に保管し直す
             if (scores[j+1] < scores[j]) {
                 num =  scores[j];
                 scores[j] = scores[j+1];
--- report7_2.c
+++ report7_2.c
@@ -13,7 +13,7 @@ int main(void){
     for (i=0; i<6; ++i) {
         for (j=0; j<6; ++j) {
             //数字を比較し、数字を降べきの順に並び直す
-            //このif文によって適当に入力された数値を降べきの順に整理されたものをnumberという配列に保管し直す
+            //このif文によって適当に入力された数値を降べきの順に整理されたものをnumという配列に保管し直す
             if (scores[j+1] < scores[j]) {
                 num =  scores[j];
                 scores[j] = scores[j+1];
